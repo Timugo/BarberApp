5faed0261303e72cbf60977bd87096a876681fa9
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-02-17 21:46:59 -0500

    slide-drawer component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,17 +4,20 @@ import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
 import { MenuComponent } from './menu/menu.component';
 import { RouterModule } from '@angular/router';
+import { SlideDrawerComponent } from './slide-drawer/slide-drawer.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    SlideDrawerComponent
   ],
   exports: [
     HeaderComponent,
     MenuComponent,
+    SlideDrawerComponent
   ],
   imports: [
     CommonModule,
