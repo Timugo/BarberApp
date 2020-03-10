a543c9a4426ac739cae88a0f89d704991a273f68
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-03-10 00:18:15 -0500

    feature(documentation in the first page )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-
 import { NavController } from '@ionic/angular';
-import { Plugins } from '@capacitor/core';
 import { CurrentOrderService } from './services/current-order.service';
+import { Plugins } from '@capacitor/core';
 
 
 const { SplashScreen } = Plugins
@@ -24,15 +23,24 @@ export class AppComponent {
     
     const ret = await Storage.get({ key: 'barber' });
     const user = JSON.parse(ret.value);
+    //if exists a barber save in the local storage
     if(user){
+      // check if this barber is enrolled in a current order on progress
       this.currentorderService.validateIfExistsOrder(user.idBarber).subscribe(res =>{
+        //barber is in a current order
         if(res['response'] == 2){
+          //navigate to the current order page
           this.navCtrl.navigateRoot('/current-order',{animated:true});
         }else{
-          this.navCtrl.navigateRoot('/orders',{animated:true});  
+          // if barber doesnt has any order in progress 
+          if(res['response'] == 1){
+            // then navigate to order page to search orders
+            this.navCtrl.navigateRoot('/orders',{animated:true});  
+          }
         }
       });  
     }else{
+      // if doesnt exists any registered barber then navigate to login page
       this.navCtrl.navigateRoot('/home',{animated:true});  
     }
     
