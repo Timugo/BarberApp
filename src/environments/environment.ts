9ba1ca3fcac7e5a7373f71c38bd72ff1578e0484
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-04-11 19:22:32 -0500

    feat( upgrade dependecies and envs)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url : 'https://www.timugotest.xyz',
+  url : 'https://sandbox.timugo.com',
   //url : 'http://localhost:3000',
   message : "DEVELOPMENT MODE"
 };
